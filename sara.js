4d1b4569d920c7a45231fa0299ffd0d4b10b8c3c
hernandessara-star <hernandes.sara@escola.pr.gov.br>
2025-11-04 08:15:58 -0300

    Create sara.js

---
+++ sara.js
@@ -0,0 +1,11 @@
+Const avanca=Document.QuerySelectorAll("ptn-proximo"):
+Avanca.ForEach(button =>{
+  button.addEventListerner('click,Function-()
+                           Const atual=Document.QuerySelector(.'Ativo');
+  Const proximoPasso='passo-'+this.Getatribute-('Data-proximo');
+  Atual Classilist.remove('ativo');
+  Document.GetElementById(proximoPasso).classlist.add.('Ativo');
+})
+})
+                          
+  
